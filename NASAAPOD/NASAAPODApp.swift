d8717843add00971c8ceea6987747c76db832c4e
PR0M4XIMUS <125589845+PR0M4XIMUS@users.noreply.github.com>
2025-04-22 18:59:22 +0300

    Initial Commit

---
+++ NASAAPOD/NASAAPODApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAAPODApp.swift
+//  NASAAPOD
+//
+//  Created by Adrian Saca on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct NASAAPODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
