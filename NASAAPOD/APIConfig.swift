0e9bb80e0170523897dd7881ec0dec282eb82163
PR0M4XIMUS <125589845+PR0M4XIMUS@users.noreply.github.com>
2025-04-22 19:25:32 +0300

    nothing important

--- NASAAPOD/APIConfig.swift
+++ NASAAPOD/APIConfig.swift
@@ -2,5 +2,5 @@ import Foundation
 
 struct APIConfig {
     // Replace with your NASA API key before running the app
-    static let nasaAPIKey = "YOUR_NASA_API_KEY"
+    static let nasaAPIKey = "b2eYNpJpZoNAaWK2rRwYInbiX1jHeLpitzy30lfc"
 }
